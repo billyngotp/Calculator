c84438a685d6a735e3c63465cbd93fc080f89ce7
Billy Ngo <billyngo@talentpath.com>
2019-11-12 15:44:31 -0600

    add describe

--- calculator.test.js
+++ calculator.test.js
@@ -1,30 +1,34 @@
 const calculator = require('./calculator')
 
-test('test equals in calculator', () => {
-    const subject = new calculator(5);
-    expect(subject.equals()).toBeDefined();
-});
+describe('calculator', () => {
+    test('test equals in calculator', () => {
+        const subject = new calculator(5);
+        expect(subject.equals()).toBeDefined();
+    });
 
-test('test adding in calculator', () => {
-    const subject = new calculator(5);
-    expect(subject.add(5).add(10).equals()).toEqual(20);
-});
+    test('test adding in calculator', () => {
+        const subject = new calculator(10);
+        expect(subject.add(5).add(10).equals()).toEqual(25);
+    });
 
-test('test subtraction in calculator', () => {
-    const subject = new calculator(5);
-    expect(subject.subtract(5).equals()).toEqual(0);
-});
+    test('test subtraction in calculator', () => {
+        const subject = new calculator(5);
+        expect(subject.subtract(5).equals()).toEqual(0);
+    });
 
-test('test multiplication in calculator', () => {
-    const subject = new calculator(5);
-    expect(subject.multiply(5).equals()).toEqual(25);
-});
-test('test division in calculator', () => {
-    const subject = new calculator(5);
-    expect(subject.divide(5).equals()).toEqual(1);
-});
+    test('test multiplication in calculator', () => {
+        const subject = new calculator(5);
+        expect(subject.multiply(5).equals()).toEqual(25);
+    });
 
-test('test clearing in calculator', () => {
-    const subject = new calculator(5);
-    expect(subject.clear().multiply(10).equals()).toEqual(0);
-});
+    test('test division in calculator', () => {
+        const subject = new calculator(5);
+        expect(subject.divide(5).equals()).toEqual(1);
+    });
+
+    test('test clearing in calculator', () => {
+        const subject = new calculator(5);
+        expect(subject.clear().multiply(10).equals()).toEqual(0);
+    });
+
+})
